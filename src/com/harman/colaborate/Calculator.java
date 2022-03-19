2e68240fb56ff773414fdee5b4e93583cc45a2b5
likhithar2507 <likhithagowda2507@gmail.com>
2022-03-19 22:18:45 +0530

    add

---
+++ src/com/harman/colaborate/Calculator.java
@@ -0,0 +1,12 @@
+package com.harman.colaborate;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+
+        int x = 5;
+        int y = 7;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
